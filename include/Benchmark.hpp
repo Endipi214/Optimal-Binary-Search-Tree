d3aba4fe8a2760fce63e1c7f979e1525121d3194
Endipi <dp01012021@gmail.com>
2025-05-20 13:56:46 +0700

    Finish BenchMark

---
+++ include/Benchmark.hpp
@@ -0,0 +1,14 @@
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "OptimalBinarySearchTree.hpp"
+
+namespace benchmarking {
+std::vector<int> vectorGenerator(size_t size, int minVal = 0, int maxVal = 100);
+std::pair<double, OBST> calExecutionTime(
+    const std::vector<int>& key, const std::vector<int>& successWeight,
+    const std::vector<int>& failureWeight, const std::string& algoName,
+    const std::function<void(OBST&)>& algo);
+void benchmark(int times, int inputSizes);
+}  // namespace benchmarking
---
+++ source/Benchmark.cpp
@@ -0,0 +1,130 @@
+#include "Benchmark.hpp"
+
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <utility>
+
+std::vector<int> benchmarking::vectorGenerator(size_t size, int minVal,
+                                               int maxVal) {
+  std::mt19937 gen(std::random_device{}());
+  std::uniform_int_distribution<int> dist(minVal, maxVal);
+  std::vector<int> vec(size);
+  for (auto& num : vec) {
+    num = dist(gen);
+  }
+  return vec;
+}
+
+std::pair<double, OBST> benchmarking::calExecutionTime(
+    const std::vector<int>& key, const std::vector<int>& successWeight,
+    const std::vector<int>& failureWeight, const std::string& algoName,
+    const std::function<void(OBST&)>& algo) {
+  auto start = std::chrono::high_resolution_clock::now();
+  OBST tree(key, successWeight, failureWeight, [&]() { algo(tree); });
+  tree.buildTree();
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duration = end - start;
+  return {duration.count(), std::move(tree)};
+}
+
+void benchmarking::benchmark(int times, int inputSizes) {
+  bool printTree = inputSizes <= 20;
+  std::cout << "===== OBST Benchmarking =====\n";
+  std::cout << "Input size: " << inputSizes << ", Repetitions: " << times
+            << "\n\n";
+
+  int printRange;
+  if (printTree)
+    printRange = inputSizes;
+  else
+    printRange = 20;
+
+  for (int i = 0; i < times; ++i) {
+    std::cout << "----- Test Run " << (i + 1) << " -----\n";
+
+    std::vector<int> key;
+    std::vector<int> successWeight;
+    std::vector<int> failureWeight;
+
+    for (int j = 0; j < inputSizes; ++j) {
+      key.push_back((j + 1) * 10);
+    }
+    successWeight = vectorGenerator(inputSizes);
+    failureWeight = vectorGenerator(inputSizes + 1);
+
+    std::cout << std::left << std::setw(17) << "Input Keys";
+    std::cout << "[";
+    std::cout << std::setw(4) << "";
+    for (size_t t = 0; t < printRange; ++t) {
+      std::cout << std::right << std::setw(4) << key[t];
+    }
+    if (printRange < inputSizes)
+      std::cout << std::right << std::setw(4) << "...";
+    std::cout << "]" << std::endl;
+
+    std::cout << std::left << std::setw(17) << "Success Weights";
+    std::cout << "[";
+    std::cout << std::setw(4) << "";
+    for (size_t t = 0; t < printRange; ++t) {
+      std::cout << std::right << std::setw(4) << successWeight[t];
+    }
+    if (printRange < inputSizes)
+      std::cout << std::right << std::setw(4) << "...";
+    std::cout << "]" << std::endl;
+
+    std::cout << std::left << std::setw(17) << "Failure Weights";
+    std::cout << "[";
+    for (size_t t = 0; t < printRange + 1; ++t) {
+      std::cout << std::right << std::setw(4) << failureWeight[t];
+    }
+    if (printRange < inputSizes)
+      std::cout << std::right << std::setw(4) << "...";
+    std::cout << "]" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << std::fixed << std::setprecision(6);
+    if (!printTree)
+      std::cout << "Skipping tree printout for inputs larger than 20 elements."
+                << std::endl
+                << std::endl;
+
+    auto [memoTime, memoTree] =
+        calExecutionTime(key, successWeight, failureWeight, "Memoization",
+                         [](OBST& tree) { tree.memoization(); });
+    memoTree.buildTree();
+    if (printTree) {
+      std::cout << "[Memoization Tree]\n";
+      memoTree.printTree();
+    }
+    std::cout << "Memoization tree cost: " << memoTree.getTreeSearchCost()
+              << "\n";
+    std::cout << "Memoization time: " << memoTime << " seconds\n\n";
+
+    auto [tabTime, tabTree] =
+        calExecutionTime(key, successWeight, failureWeight, "Tabulation",
+                         [](OBST& tree) { tree.tabulation(); });
+    tabTree.buildTree();
+    if (printTree) {
+      std::cout << "[Tabulation Tree]\n";
+      tabTree.printTree();
+    }
+    std::cout << "Tabulation tree cost: " << tabTree.getTreeSearchCost()
+              << "\n";
+    std::cout << "Tabulation time: " << tabTime << " seconds\n\n";
+
+    auto [knuthTime, knuthTree] =
+        calExecutionTime(key, successWeight, failureWeight, "Knuth Algorithm",
+                         [](OBST& tree) { tree.knuthAlgo(); });
+    knuthTree.buildTree();
+    if (printTree) {
+      std::cout << "[Knuth Algorithm Tree]\n";
+      knuthTree.printTree();
+    }
+    std::cout << "Knuth Algorithm tree cost: " << knuthTree.getTreeSearchCost()
+              << "\n";
+    std::cout << "Knuth Algorithm time: " << knuthTime << " seconds\n";
+    std::cout << "----------------------------\n\n";
+  }
+}
