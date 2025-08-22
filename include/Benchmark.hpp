f2b23bc1bb7e14f917200485dad50b74e0bfc225
Endipi <dp01012021@gmail.com>
2025-08-23 01:57:01 +0700

    Batmannnnn

--- include/Benchmark.hpp
+++ include/Benchmark.hpp
@@ -7,9 +7,9 @@
 namespace benchmarking {
 std::vector<int> vectorGenerator(size_t size, int minVal = 0, int maxVal = 100);
 std::pair<double, OBST> calExecutionTime(
-    const std::vector<int>& key, const std::vector<int>& successWeight,
-    const std::vector<int>& failureWeight, const std::string& algoName,
-    const std::function<void(OBST&)>& algo);
+    const std::vector<int> &key, const std::vector<int> &successWeight,
+    const std::vector<int> &failureWeight, const std::string &algoName,
+    const std::function<void(OBST &)> &algo);
 void benchmark(int times, int inputSizes, std::vector<int> successWeight = {},
                std::vector<int> failureWeight = {});
-}  // namespace benchmarking
+} // namespace benchmarking
